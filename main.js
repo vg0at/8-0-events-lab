3fbff1c1b519dd5aed947aec4d6e65a863b55129
vg0at <cameronmedina0130@gmail.com>
2022-12-15 22:37:47 -0500

    Initial commit

--- main.js
+++ main.js
@@ -7,3 +7,46 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+// should change the background of #current-color div when a color is clicked
+/* 
+  example 
+  const buttons = document.querySelectorAll("button");
+for (let button of buttons) {
+  button.addEventListener("click", () => {
+    console.log("A button was clicked!");
+  });
+}
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log("A button was clicked!");
+});
+}
+*/
+
+const paletteColor = document.querySelectorAll("#palette .color");
+
+// console.log(paletteColor)
+
+paletteColor.forEach((color) => {
+  color.addEventListener("click", () => {
+    const chosenColor = document.querySelector('#current-color')
+    chosenColor.style.backgroundColor = color.style.backgroundColor;
+  });
+
+});
+
+// should change cells to the currently selected color when clicked
+/*
+iterate through all cells 
+add event listener: if cell is clicked, cell background color should be current color.
+
+ */
+const cells = document.querySelectorAll(".cell")
+const current = document.querySelector("#current-color")
+cells.forEach((cell) => {
+  cell.addEventListener("click", () => {
+    cell.style.backgroundColor = current.style.backgroundColor;
+  });
+})
